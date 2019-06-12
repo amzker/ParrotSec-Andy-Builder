764ee32bc04c04a9dbb6d1aa74dfb0333e19e7fc
amzker pro hacker <35922655+amzker@users.noreply.github.com>
2019-06-11 23:22:09 -0500

    push new

---
+++ common/inme.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt update && apt update && apt update
+apt install $(cat /home/parrot/pack.list) -y
+apt install $(cat /home/parrot/pack.list) -y
+patch1.Amzhook
+cleanup.Amzhook
+echo "MY WORK IS DONE!!, Now Your Turn For Modification; Install Remove chnage as you want"
+echo "After you done ; exit from shell & run build"
